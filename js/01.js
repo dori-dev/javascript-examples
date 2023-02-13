a3d0867df291ec6446449dfb8008756e9192bcfd
dori-dev <mr.dori.dev@gmail.com>
2023-02-13 21:06:42 +0330

    remove codes

--- js/01.js
+++
@@ -1,5 +0,0 @@
-// This is comment
-let string = "Hello, world!";
-console.log(string);
-document.write(string);
-alert(string);
--- js/02.js
+++
@@ -1,9 +0,0 @@
-var field = "Backend";
-console.log(field);
-field = "Frontend";
-console.log(field);
-
-const access = "yes";
-console.log(access);
-access = "no";
-console.log(access);
