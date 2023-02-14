92b66f812fa0261382edd5d7c650d4514eefc33a
dori-dev <mr.dori.dev@gmail.com>
2023-02-14 19:47:14 +0330

    use logical operators

--- conditions.js
+++ conditions.js
@@ -1,8 +1,8 @@
 let mohammad = [20, true];
 
-if (mohammad[0] > 15) {
+if (mohammad[0] > 15 && mohammad[1]) {
   console.log("Your point is great!");
-} else if (mohammad[0] > 10) {
+} else if (mohammad[0] > 10 || mohammad[1]) {
   console.log("Your point is not too bad.");
 } else {
   console.log("Your point is bad.");
