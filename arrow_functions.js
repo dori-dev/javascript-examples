e64c8c3cbb30783bbfdb02bf014b16611ce6ff30
dori-dev <mr.dori.dev@gmail.com>
2023-02-14 22:44:43 +0330

    use arrow functions

---
+++ arrow_functions.js
@@ -0,0 +1,12 @@
+const sayHello = () => {
+  console.log("Hello");
+};
+
+const add = (a, b) => a + b;
+
+sayHello();
+console.log(add(2, 6));
+
+let numbers = [1, 5, 2, 34, 6, 78, 8, 4, 8, 42];
+
+numbers.forEach((num) => console.log(num));
