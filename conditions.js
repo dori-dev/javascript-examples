7308565686ecb0c351da25f5e24637bdf5fbbe35
dori-dev <mr.dori.dev@gmail.com>
2023-02-14 19:27:05 +0330

    use condition

---
+++ conditions.js
@@ -0,0 +1,9 @@
+let mohammad = [20, true];
+
+if (mohammad[0] > 15) {
+  console.log("Your point is great!");
+} else if (mohammad[0] > 10) {
+  console.log("Your point is not too bad.");
+} else {
+  console.log("Your point is bad.");
+}
