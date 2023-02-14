796be2d13d9ae50fa3268e737800faddb99f3fb6
dori-dev <mr.dori.dev@gmail.com>
2023-02-14 21:04:58 +0330

    use if in while

--- while.js
+++ while.js
@@ -1,6 +1,18 @@
 let number = 0;
+let sum = 0;
 
 while (number < 100) {
-  console.log(`number: ${number}`);
   number++;
+  if (number >= 50) {
+    break;
+  }
+  if (number in [2, 6, 10, 12]) {
+    continue;
+  }
+  if (number % 2 == 0) {
+    console.log(`number: ${number}`);
+    sum += number;
+  }
 }
+
+console.log(`Sum of numbers: ${sum}`);
