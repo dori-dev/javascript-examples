928120b10937a62ab31e2c31e2638598d9ef6ee2
dori-dev <mr.dori.dev@gmail.com>
2023-02-14 22:15:37 +0330

    create function for is primary

--- is_primary.js
+++ is_primary.js
@@ -1,11 +1,15 @@
-let n = 1000000007;
-let isPrimary = true;
-
-for (let i = 2; i <= Math.floor(n ** 0.5) + 1; i++) {
-  if (n % i == 0) {
-    isPrimary = false;
-    break;
+function isPrimary(n) {
+  let isPrimary = true;
+  for (let i = 2; i <= Math.floor(n ** 0.5) + 1; i++) {
+    if (n % i == 0) {
+      isPrimary = false;
+      break;
+    }
   }
+  return isPrimary;
 }
 
-console.log(n, isPrimary);
+console.log(isPrimary(1000000007));
+console.log(isPrimary(1000000008));
+console.log(isPrimary(2587));
+console.log(isPrimary(1000000021));
