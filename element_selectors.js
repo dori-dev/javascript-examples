d10bedf34e8dadbdb35035e95964b9442cf559a9
dori-dev <mr.dori.dev@gmail.com>
2023-02-16 00:01:16 +0330

    get element

---
+++ element_selectors.js
@@ -0,0 +1,7 @@
+let elementById = document.getElementById("elementId");
+let elementByClassName = document.getElementsByClassName("elementClassName");
+let elementByTagName = document.getElementsByTagName("elementTagName");
+
+// Better Way:
+let element = document.querySelector("elementQuery");
+let elements = document.querySelectorAll("elementsQuery");
