5201e264601d6cae2cbefac12a9c73a179e244c5
dori-dev <mr.dori.dev@gmail.com>
2023-02-17 12:28:46 +0330

    add animation to accordions

--- accordion-menu/scripts.js
+++ accordion-menu/scripts.js
@@ -1,15 +1,29 @@
+const PADDING = 10 * 2;
+
+function setMaxHeight(wrapper, contentDiv) {
+  if (wrapper.classList.contains("show")) {
+    contentDiv.style.maxHeight = `${contentDiv.scrollHeight + PADDING}px`;
+  } else {
+    contentDiv.style.maxHeight = null;
+  }
+}
+
 function closeAccordion(children, target) {
   children.forEach((wrapper) => {
     if (wrapper != target) {
       wrapper.classList.remove("show");
+      wrapper.querySelector("div").style.maxHeight = null;
     }
   });
 }
 
 function setAccordion(children) {
   children.forEach((wrapper) => {
+    let contentDiv = wrapper.querySelector("div");
+    setMaxHeight(wrapper, contentDiv);
     wrapper.querySelector("span").addEventListener("click", (e) => {
       wrapper.classList.toggle("show");
+      setMaxHeight(wrapper, contentDiv);
       closeAccordion(children, wrapper);
     });
   });
