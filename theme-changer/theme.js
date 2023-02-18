3dce4cbc7f1129f41d9759918d58d6eccffa9d5d
dori-dev <mr.dori.dev@gmail.com>
2023-02-18 15:08:51 +0330

    use local storage to save theme

--- theme-changer/theme.js
+++ theme-changer/theme.js
@@ -1,8 +1,14 @@
 let theme = document.querySelector(".theme");
+let currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : "light";
+
+document.body.className = currentTheme;
 
 Array.from(theme.children).forEach((child) => {
   child.addEventListener("click", (e) => {
     let themeColor = e.target.dataset.color;
     document.body.className = themeColor;
+    localStorage.setItem("theme", themeColor);
   });
 });
