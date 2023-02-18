990df5f8b6720e63d6b6618f6c7d8dd6e8d59a33
dori-dev <mr.dori.dev@gmail.com>
2023-02-18 16:27:28 +0330

    add update todo

--- todos/script.js
+++ todos/script.js
@@ -1,4 +1,8 @@
 let todos = localStorage.getItem("todos");
+let todosList = document.querySelector("#todosList");
+let sampleTodo = document.querySelector("#sampleTodo").cloneNode(true);
+sampleTodo.hidden = false;
+sampleTodo.removeAttribute("id");
 
 try {
   todos = JSON.parse(todos);
@@ -11,3 +15,14 @@ if (!todos) {
   todos = ["Shopping", "Watch videos", "Sport"];
   localStorage.setItem("todos", JSON.stringify(todos));
 }
+
+function updateTodos(todos) {
+  todosList.innerHTML = "";
+  todos.forEach((todo) => {
+    let li = sampleTodo.cloneNode(true);
+    li.querySelector("span").textContent = todo;
+    todosList.prepend(li);
+  });
+}
+
+updateTodos(todos);
