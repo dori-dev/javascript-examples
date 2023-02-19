23e0ce2dac9d63a2f0c6d0239a4fdedc17f075e4
dori-dev <mr.dori.dev@gmail.com>
2023-02-19 21:47:36 +0330

    create clock

---
+++ clock/script.js
@@ -0,0 +1,8 @@
+function myClock() {
+  let clock = document.querySelector("#clock");
+  let myDate = new Date();
+  clock.textContent = `${myDate.getHours()}:${myDate.getMinutes()}:${myDate.getSeconds()}`;
+}
+
+myClock();
+setInterval(myClock, 1000);
