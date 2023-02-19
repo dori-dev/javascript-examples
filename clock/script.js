711845390a5df2a9f4e2566fba97a6bca1020ef0
dori-dev <mr.dori.dev@gmail.com>
2023-02-19 22:20:01 +0330

    add jalali date to clock

--- clock/script.js
+++ clock/script.js
@@ -1,7 +1,24 @@
+let clock = document.querySelector("#clock");
+let date = document.querySelector("#date");
+let myDate = new Date();
+let jDate = gregorian_to_jalali(
+  myDate.getFullYear(),
+  myDate.getMonth() + 1,
+  myDate.getDate()
+);
+
 function myClock() {
-  let clock = document.querySelector("#clock");
-  let myDate = new Date();
-  clock.textContent = `${myDate.getHours()}:${myDate.getMinutes()}:${myDate.getSeconds()}`;
+  myDate = new Date();
+  let hour = myDate.getHours().toString().padStart(2, "0");
+  let minute = myDate.getMinutes().toString().padStart(2, "0");
+  let second = myDate.getSeconds().toString().padStart(2, "0");
+  clock.textContent = `${hour}:${minute}:${second}`;
+  jDate = gregorian_to_jalali(
+    myDate.getFullYear(),
+    myDate.getMonth() + 1,
+    myDate.getDate()
+  );
+  date.textContent = `${jDate[0]}/${jDate[1]}/${jDate[2]}`;
 }
 
 myClock();
