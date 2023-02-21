fb0816eba7c95393f56000e5f410d6380a1e1e93
dori-dev <mr.dori.dev@gmail.com>
2023-02-21 17:02:23 +0330

    add weather app

---
+++ weather/script.js
@@ -0,0 +1,31 @@
+let form = document.querySelector("form");
+let token = "221783:63f4a8277152a";
+let url = `https://one-api.ir/weather/?token=${token}&action=current&city=`;
+let iconUrl = `https://one-api.ir/weather/?token=${token}&action=icon&id=`;
+let result = document.querySelector("#result");
+let showCity = document.querySelector("#showCity");
+let showDescription = document.querySelector("#showDescription");
+let weatherIcon = document.querySelector(".weather-wrapper img");
+let maxTemp = document.querySelector("#maxTemp .deg");
+let temp = document.querySelector("#temp .deg");
+let minTemp = document.querySelector("#minTemp .deg");
+
+async function getWeather(city) {
+  let response = await fetch(url + city);
+  return await response.json();
+}
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  let value = e.target.city.value;
+  if (value) {
+    let result = await getWeather(value);
+    result = result.result;
+    showCity.textContent = value;
+    showDescription.textContent = result.weather[0].description;
+    weatherIcon.src = iconUrl + result.weather[0].icon;
+    maxTemp.textContent = Math.round(result.main.temp_max);
+    temp.textContent = Math.round(result.main.temp);
+    minTemp.textContent = Math.round(result.main.temp_min);
+  }
+});
